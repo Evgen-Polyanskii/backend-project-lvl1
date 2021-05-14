012509adda8901205e9373807614ad2792ddd973
Polyansky <m4x1morlov@yandex.ru>
2021-05-14 17:39:56 +0300

    Fix src/checkForParity.js

--- src/checkForParity.js
+++ src/checkForParity.js
@@ -1,16 +1,18 @@
 import readlineSync from 'readline-sync';
 
-const isEvenNumber = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
+const isEvenNumber = (number) => (number % 2 === 0 ? 'yes' : 'no');
+
+const getRundomNumber = () => {
+  const minValue = 1;
+  const maxValue = 100;
+  const rundomNumber = Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+  return rundomNumber;
 };
 
 const checkUserAnswer = (name) => {
-  const rundomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-  const userAnsver = readlineSync.question(`Question: ${rundomNumber} `);
-  const correctAnswer = isEvenNumber(rundomNumber);
+  const num = getRundomNumber();
+  const userAnsver = readlineSync.question(`Question: ${num} `);
+  const correctAnswer = isEvenNumber(num);
   if (correctAnswer === userAnsver) {
     console.log('Correct!');
     return true;
@@ -25,8 +27,8 @@ const gameCheckForParity = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let i = 1;
-  for (i; i <= 3; i += 1) {
+  let numOfRounds = 1;
+  for (numOfRounds; numOfRounds <= 3; numOfRounds += 1) {
     if (checkUserAnswer(userName) === false) {
       return;
     }
