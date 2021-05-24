dd2d2369235bbc95734aea1fae5d094dd9e150cb
Polyansky <m4x1morlov@yandex.ru>
2021-05-24 11:23:12 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const startGames = (description, gameData) => {
       console.log('Correct!');
     } else {
       console.log(`${userAnswer} is wrong answer ;(, Correct answer was ${correctAnswer}`);
-      console.log(`Let's try again, ${userName}`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
