cf028d4435e72463f7e08aca498b784e7a7cd309
Polyansky <m4x1morlov@yandex.ru>
2021-05-24 19:07:15 +0300

    Fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,13 +3,13 @@ import startGames from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const getAnswer = (num) => {
+const isPrime = (num) => {
   for (let i = 2; i < num / 2; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const getGameData = () => {
@@ -18,7 +18,7 @@ const getGameData = () => {
   const maxValue = 100;
   const number = getRundomNumber(minValue, maxValue);
   gameData.push(number);
-  const answer = getAnswer(number);
+  const answer = isPrime(number) ? 'yes' : 'no';
   gameData.push(answer);
   return gameData;
 };
