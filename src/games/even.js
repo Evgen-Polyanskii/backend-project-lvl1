8c131b73e362d767bceb24411676eb4976f6114f
Polyansky <m4x1morlov@yandex.ru>
2021-05-24 19:06:31 +0300

    Fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,14 +3,14 @@ import startGames from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEvenNumber = (number) => (number % 2 === 0 ? 'yes' : 'no');
+const isEvenNumber = (number) => number % 2 === 0;
 
 const getGameData = () => {
   const gameData = [];
   const minValue = 1;
   const maxValue = 100;
   const num = getRundomNumber(minValue, maxValue);
-  const correctAnswer = isEvenNumber(num);
+  const correctAnswer = isEvenNumber(num) ? 'yes' : 'no';
   gameData.push(num, correctAnswer);
   return gameData;
 };
